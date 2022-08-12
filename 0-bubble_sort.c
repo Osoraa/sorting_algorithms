cf124cce8408b43cf96186198bebf7ce4d414dd4
Osoraa <sochi98.sn@gmail.com>
2022-08-12 15:28:21 +0100

    Task 0 - Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] < array[j + 1])
+			if (array[j] <= array[j + 1])
 				continue;
 			temp = array[j];
 			array[j] = array[j + 1];
