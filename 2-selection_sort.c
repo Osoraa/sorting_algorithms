11aa75341028a79b05b44a4b49ef27a97bf38510
Osoraa <sochi98.sn@gmail.com>
2022-08-15 03:56:14 +0100

    Task 2 - Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order in place.
+ *
+ * @array: Array of integers.
+ * @size: Size of array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int least, index, temp, *current = array;
+	size_t i, count = 0, _size = size;
+
+	if (!array)
+		return;
+
+	while (count++ < size)
+	{
+		least = current[0];
+
+		for (i = 1; i < _size; i++)
+		{
+			if (least > current[i])
+			{
+				least = current[i];
+				index = i;
+			}
+		}
+
+		if (least == current[0])
+		{
+			current++;
+			continue;
+		}
+
+		temp = current[0];
+		current[0] = least;
+		current[index] = temp;
+
+		print_array(array, size);
+		current++;
+		_size--;
+	}
+}
